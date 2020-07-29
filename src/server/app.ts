46ada741763b4b628c0bbe3a867402e8f9c920bc
Richard Lake <ralake1985@gmail.com>
2020-07-29 14:27:55 +0100

    Switch order

--- src/server/app.ts
+++ src/server/app.ts
@@ -15,11 +15,11 @@ const io = createSocket(http)
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get('/games/:gameId', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public', 'index.html'))
 })
 
-app.get('/games/:gamedId', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public', 'index.html'))
 })
 
