469a2fbcea23a9460ff80cf5c46b59af5f48d804
Richard Lake <ralake1985@gmail.com>
2020-07-29 12:20:20 +0100

    Two routes

--- src/server/app.ts
+++ src/server/app.ts
@@ -18,7 +18,11 @@ const router = Router()
 app.use(router)
 app.use(express.static('public'))
 
-app.get(['/', '/games/:gamedId'], (req, res) => {
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public', 'index.html'))
+})
+
+app.get('/games/:gamedId', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public', 'index.html'))
 })
 
