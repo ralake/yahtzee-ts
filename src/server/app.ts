00903de6c884c3f29035a362e51fb97f9893a524
Richard Lake <ralake1985@gmail.com>
2020-07-29 14:52:09 +0100

    eh?

--- src/server/app.ts
+++ src/server/app.ts
@@ -15,11 +15,12 @@ const io = createSocket(http)
 
 app.use(express.static('public'))
 
-app.get('/games/:gameId', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public', 'index.html'))
-})
+// app.get('/games/:gameId', (req, res) => {
+//   console.log(path.join(__dirname, '../../public', 'index.html'))
+//   res.sendFile(path.join(__dirname, '../../public', 'index.html'))
+// })
 
-app.get('/', (req, res) => {
+app.get('/games/:gameId', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public', 'index.html'))
 })
 
